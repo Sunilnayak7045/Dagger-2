1775226cb75ef9c2fde5b663b1b3be81446b92e8
sunilnayak7045 <sunilnayak7045@gmail.com>
2022-06-07 11:57:25 +0530

    Field Injection

--- app/src/main/java/com/example/dagger/MainActivity.kt
+++ app/src/main/java/com/example/dagger/MainActivity.kt
@@ -10,6 +10,9 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var userRegistrationService: UserRegistrationService
 
+    @Inject
+    lateinit var emailService: EmailService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
